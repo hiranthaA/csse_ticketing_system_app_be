c93d680547aeb44e381f1226673caa39c7e32282
hiranthaA <30893182+hiranthaA@users.noreply.github.com>
2018-09-30 19:03:51 +0530

    customer app initial backend

---
+++ src/main/java/com/csse/travelpay/TravelpayApplication.java
@@ -0,0 +1,12 @@
+package com.csse.travelpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelpayApplication.class, args);
+	}
+}
